c4cf9121d260819d52218fd4139dcf1c54cdcaa1
vjdodge <valoriejones@gmail.com>
2016-04-25 09:34:42 -0700

    Assignment 46 complete

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -36,6 +36,18 @@ RSpec.describe Topic, type: :model do
                 expect(Topic.visible_to(nil)).to eq([@public_topic])
             end
         end
+        
+        describe "publicly_viewable" do
+            it "returns all public topics" do
+                expect(Topic.publicly_viewable).to eq([@public_topic])
+            end
+        end
+        
+        describe "privately_viewable" do
+            it "returns all private topics" do
+                expect(Topic.privately_viewable).to eq([@private_topic])
+            end
+        end
     end
 
 end
